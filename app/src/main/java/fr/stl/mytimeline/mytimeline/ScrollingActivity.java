97a3f2b77f71ee208449aaaf5817469b1156e75e
LC laptop <lingchun.so@gmail.com>
2018-10-16 12:27:46 +0200

    push

--- app/src/main/java/fr/stl/mytimeline/mytimeline/ScrollingActivity.java
+++ app/src/main/java/fr/stl/mytimeline/mytimeline/ScrollingActivity.java
@@ -49,17 +49,7 @@ public class ScrollingActivity extends AppCompatActivity {
             }
         });
 
-        ImageButton imgbtn = findViewById(R.id.imgbtn);
-        imgbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
 
-            }
-        });
 
 
 
