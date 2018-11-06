a0d3803aae915a76ffea264b091fb3ba06ff7fe4
chao450 <lin.chao.xz@gmail.com>
2018-11-06 08:57:52 +0100

    Delete useless code

--- app/src/main/java/fr/stl/mytimeline/mytimeline/ScrollingActivity.java
+++ app/src/main/java/fr/stl/mytimeline/mytimeline/ScrollingActivity.java
@@ -492,25 +492,13 @@ public class ScrollingActivity extends AppCompatActivity {
         notificationManager.createNotificationChannel(channel);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_scrolling, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        
         if (id == android.R.id.home){
             drawer.openDrawer(GravityCompat.START);
         }
